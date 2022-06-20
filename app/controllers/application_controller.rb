2925373ab27d710f5d450f116a9f879d0d10c3eb
Cristina Diaz <diazcristina.la@gmail.com>
2022-06-20 16:50:17 -0700

    commit tree 9dae1c49c9051efaf74ffbae99b68a3b792b65f6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  #add this line to set the Content-Type header for all responses 
+  set :default_content_type, 'application/json' 
+
+  get '/games' do
+    # { message: "Hello world" }.to_json
+    # get all games from db
+    # return a JSON response with array of all the game data
+
+    games = Game.all
+    games.to_json
+
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(include: { reviews: { include: :user} })
   end
 
 end
